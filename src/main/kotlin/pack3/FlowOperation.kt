6fba3e2c885f95c605a7ef5ffe4c1a6095464346
Linad <merzlikindaniil97@gmail.com>
2024-10-06 15:58:02 +0300

    FlowOperation.kt is done

--- src/main/kotlin/pack3/FlowOperation.kt
+++ src/main/kotlin/pack3/FlowOperation.kt
@@ -1,8 +1,23 @@
 package org.example.pack3
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 
+val names = arrayOf(
+    "Иван",
+    "Петр",
+    "Дмитрий",
+    "София",
+    "Лера",
+    "Тимофей",
+    "Даниил",
+    "Платон",
+    "Константин",
+    "Глеб",
+    "Вероника",
+    "Матвей",
+    "Александр"
+)
 
 private fun listIntNum(length: Int, min: Int, max: Int): ArrayList<Int> {
     val result = arrayListOf<Int>()
@@ -21,19 +36,93 @@ private fun listIntNum(): ArrayList<Int> {
 }
 
 private class Person(
-    val name,
-    val age
+    val name: String,
+    val age: Int
 )
+//
+//private class PersonEx3(
+//    val name: String,
+//    val cart: String,
+//    val password: String
+//)
+
+private fun numCart(): String {
+    var result = ""
+    for (i in 1..4) {
+        for (j in 1..4) {
+            result += ('0'..'9').random().toString()
+        }
+        if (i != 4) {
+            result += " "
+        }
+    }
+    return result
+}
+
+private fun randPassword(): String {
+    var result = ""
+    for (i in 1..4) {
+        result += ('0'..'9').random().toString()
+    }
+    return result
+}
 
 suspend fun main() {
 
-    println(ex1())
+    ex1()
+
+    ex2()
+
+    ex3()
 
 
 }
 
-private suspend fun ex1(): String {
-    return flow {
+private suspend fun ex3() {
+    val addNameFlow = addNameFlow()
+    val addCartFlow = addCartFlow()
+    val addPasswordFlow = addPasswordFlow()
+    addNameFlow.zip(addCartFlow) { name, cart ->
+        "name = $name, cart = $cart, "
+    }.zip(addPasswordFlow) { nameZipCart, password ->
+        "$nameZipCart password = $password"
+    }.collect {
+        println("Person ($it),")
+    }
+}
+
+private fun addNameFlow() = flow {
+    val namesIn = arrayListOf<String>()
+    for (i in 1..10) {
+        namesIn.add(names.random())
+    }
+    namesIn.forEach {
+        emit(it)
+    }
+}
+
+private fun addPasswordFlow() = flow {
+    val passwords = arrayListOf<String>()
+    for (i in 1..10) {
+        passwords.add(randPassword())
+    }
+    passwords.forEach {
+        emit(it)
+    }
+}
+
+private fun addCartFlow() = flow {
+    val carts = arrayListOf<String>()
+    for (i in 1..10) {
+        carts.add(numCart())
+    }
+    carts.forEach {
+        emit(it)
+    }
+}
+
+private suspend fun ex1() {
+    println(flow {
         val number = listIntNum()
         println(number)
         number.forEach {
@@ -41,10 +130,33 @@ private suspend fun ex1(): String {
             emit(it)
         }
     }.map { it * it }
-        .reduce { sum, element -> sum + element }.toString()
+        .reduce { sum, element -> sum + element }.toString())
 }
-private fun getPerson(first:String, age:Int) = flow<Person> {
-    val persons = arrayListOf({
-        Person("")
-    })
+
+private suspend fun ex2() {
+    val persons = arrayListOf<Person>()
+    for (i in 1..10) {
+        persons.add(Person(names.random(), (1..60).random()))
+    }
+    println()
+    persons.forEach { println("${it.name}, ${it.age}") }
+    println()
+
+    println("Первая буква имени: ")
+    val firstCharName: Char = readln().first()
+
+    println("Введите возраст: ")
+    val agePerson: Int = readlnOrNull()?.toIntOrNull() ?: 18
+
+    getPerson(firstCharName.toString().uppercase(), agePerson, persons).collect {
+        println(it.name + " " + it.age)
+    }
+
+}
+
+private fun getPerson(first: String, age: Int, persons: ArrayList<Person>) = flow {
+    persons.asFlow().filter { person -> person.age > age && person.name.first().toString().uppercase() == first }
+        .collect {
+            emit(it)
+        }
 }
