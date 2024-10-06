ed8a20b925d1a787590b6abf15b16945db5d7cd5
Linad <merzlikindaniil97@gmail.com>
2024-10-06 13:41:54 +0300

    Add HW in Functions-of-streams.kt

--- src/main/kotlin/Functions-of-streams.kt
+++ src/main/kotlin/Functions-of-streams.kt
@@ -1,13 +1,58 @@
 package org.example
-//
-//val SELECT_CHAR = arrayListOf(('a'..'z').toMutableList().toString())
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.withContext
+import kotlin.system.measureTimeMillis
+
 val SELECT = ('0'..'9').toList() + ('a'..'z').toList()
+const val SIZE_LIST_ID = 6
 
+suspend fun main() {
+    println("Кол-во пользователей: ")
+    val countPerson: Int = readlnOrNull()?.toIntOrNull() ?: 100
 
-fun main() {
-    repeat(15) {
-        println(createPassword())
+    println("Пароли с символа: ")
+    val charPassword: Char = readln().first()
+
+    val listPassword = arrayListOf<String>()
+    val listId = arrayListOf<String>()
+
+    val time = measureTimeMillis {
+        withContext(newSingleThreadContext("my_Thread")) {
+            val passwordJob = launch {
+                getPasswordFlow(
+                    charPassword.toString(),
+                    countPerson
+                )
+                    .collect {
+                        println("Add password")
+                        listPassword.add(it)
+                    }
+            }
+            val idFlow = launch {
+                getIdFlow(countPerson)
+                    .collect {
+                        println("Add user id[$it]")
+                        listId.add(it)
+                    }
+            }
+            passwordJob.join()
+            idFlow.join()
+        }
+
+        val result = mutableMapOf<String, String>()
+        for (i in listId.indices) {
+            result[listId[i]] = listPassword[i]
+        }
+        result.forEach {
+            println("${it.key} = ${it.value}")
+        }
     }
+    println("На создание $countPerson потребовалось $time мс")
 }
 
 fun createPassword(): String {
@@ -36,3 +81,43 @@ fun isNum(char: String): Boolean {
     }
 }
 
+fun getListOfPassword(input: String, length: Int): List<String> {
+    val result = mutableListOf<String>()
+    while (result.size < length) {
+        var password = createPassword()
+        while (password[0].toString() != input) {
+            password = createPassword()
+        }
+        result.add(password)
+    }
+    return result
+}
+
+fun getListId(length: Int): List<String> {
+    val result = mutableListOf<String>()
+    var buf = ""
+    while (result.size < length) {
+        buf = (result.size + 1).toString()
+        for (i in 1..SIZE_LIST_ID - (result.size + 1).toString().length) {
+            buf = "0$buf"
+        }
+        result.add(buf)
+    }
+    return result
+}
+
+fun getIdFlow(length: Int) = flow {
+    val id = getListId(length)
+    id.forEach {
+        delay(100L)
+        emit(it)
+    }
+}
+
+fun getPasswordFlow(input: String, length: Int) = flow {
+    val password = getListOfPassword(input, length)
+    password.forEach {
+        delay(100L)
+        emit(it)
+    }
+}
